ad49896b652ec7af2ab241bf5107790bd69734dc
Brody Bayley <brodybayley@gmail.com>
2021-02-13 00:08:33 +0000

    added balance checker and updated driver code

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+class Account {
+
+  constructor() {
+    this.transactions = [];
+  }
+
+  get balance() {
+    let balance = 0;
+    for (let t of this.transactions) {
+      balance += t.value;
+    }
+    return balance;
+  }
+
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+
+}
+
+
 class Transaction {
 
   constructor(amount, account) {
@@ -5,7 +27,10 @@ class Transaction {
     this.account = account;
   }
   commit() {
-    this.account.balance += this.value;
+    if (!this.isAllowed()) return false;
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
   }
 
 }
@@ -14,6 +39,10 @@ class Deposit extends Transaction {
 
   get value() {
     return this.amount;
+  }
+
+  isAllowed() {
+    return true;
   }
 
 }
@@ -26,32 +55,30 @@ class Withdrawal extends Transaction {
 
   }
 
-}
-
-class Account {
-
-  constructor(username) {
-    this.username = username;
-    this.balance = 0;
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
   }
 
 }
+
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 const myAccount = new Account("snow-patrol");
 
+console.log('Starting Account Balance: ', myAccount.balance);
+
 let t1 = new Withdrawal(50.25, myAccount);
-t1.commit();
-console.log('Transaction 1:', t1);
+console.log('Commit Result: ', t1.commit());
+console.log('Account Balance: ', myAccount.balance);
 
-let t2 = new Withdrawal(9.99, myAccount);
-t2.commit();
-console.log('Transaction 2:', t2);
+let t2 = new Deposit(120.00, myAccount);
+console.log('Commit Result', t2.commit());
+console.log('Account Balance: ', myAccount.balance);
 
-let t3 = new Deposit(120.00, myAccount);
-t3.commit();
-console.log('Transaction 3:', t3);
+let t3 = new Withdrawal(9.99, myAccount);
+console.log('Commit Result', t3.commit());
+console.log('Account Balance: ', myAccount.balance);
 
 console.log('Balance:', myAccount.balance);
